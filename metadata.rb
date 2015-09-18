43935f12b1ff8faabc4e97333d78ba305bfa88f8
Ryan Ovrevik <rovrevik@ovrevik.com>
2015-09-17 23:07:46 -0400

    execute chef generate cookbook container_users

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'container_users'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures container_users'
+long_description 'Installs/Configures container_users'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: container_users
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
